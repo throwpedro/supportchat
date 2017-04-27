bbfe35048b6b2e4c39257383b684e3b77606c599
Thor mint <thordoor89@hotmail.com>
2017-04-27 13:27:10 +0200

    Some cleaning in index.js

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 var firstClient = true;
 var roomNo = 1;
 io.on('connection', function (socket) {
-  //console.log(io.nsps['/'].adapter.rooms["room-"+roomNo]);
   if (io.nsps['/'].adapter.rooms["room-" + roomNo] && io.nsps['/'].adapter.rooms["room-" + roomNo].length > 1) {
     //console.log(io.nsps['/'].adapter.rooms["room-"+roomNo]);
     roomNo++;
@@ -25,13 +24,7 @@ io.on('connection', function (socket) {
     firstClient = false;
   }
   console.log("socketid: " + socket.id);
-  //console.log(io.sockets.adapter.rooms);
-  //console.log(io.sockets.clients().adapter.rooms);
-  /*//talk only in your room.
-  socket.on('chat message', function(msg){
-    io.to('room-'+roomNo).emit('chat message', msg);
-  });*/
-  //send to everyone in the room
+
   io.sockets.in("room-" + roomNo).emit('connectToRoom', "room-" + roomNo);
 
 });
