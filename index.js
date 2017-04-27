102bed761e28d72de33151d014746017554cc6fe
Thor <thordoor89@hotmail.com>
2017-04-27 23:02:09 +0200

    different versions not working. and chat not working when socket.id is hrskyen

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
 
 //app.get('/', function(req, res){
 //  res.sendFile(__dirname + '/customer.html');
@@ -14,27 +14,32 @@ app.use(express.static(path.join(__dirname, 'public')));
 var firstClient = true;
 var roomNo = 1;
 io.on('connection', function (socket) {
-  if (io.nsps['/'].adapter.rooms["room-" + roomNo] && io.nsps['/'].adapter.rooms["room-" + roomNo].length > 1) {
+  console.log("hej");
+  //if (io.nsps['/'].adapter.rooms["room-" + roomNo] && io.nsps['/'].adapter.rooms["room-" + roomNo].length > 1) {
     //console.log(io.nsps['/'].adapter.rooms["room-"+roomNo]);
-    roomNo++;
-  }
-  socket.join("room-" + roomNo);
+  //  roomNo++;
+  //}
+  //socket.join("room-" + roomNo);
   if (firstClient) {
     socket.id = "hrskyen";
     firstClient = false;
   }
   console.log("socketid: " + socket.id);
-  io.sockets.in("room-" + roomNo).emit('connectToRoom', "room-" + roomNo);
+  
+  //io.sockets.in("room-" + roomNo).emit('connectToRoom', "room-" + roomNo);
 
 });
 
 io.on('connection', function (socket) {
-  socket.on('chat message', function (msg, roomName) {
-
-   
+  console.log(socket);
+  socket.on('chat message', function (msg) {
+  
+  console.log(socket.id);
+  console.log(msg);
+  //io.emit('chat message', msg);
   io.to(socket.id).emit('chat message', msg);
     
-    socket.broadcast.to(roomName).emit('chat message', msg);
+    //socket.broadcast.to(roomName).emit('chat message', msg);
   });
 });
 
