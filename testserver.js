455ff7db640d8bfd28969c7cc5d5d42e9a664b3e
Thor <thordoor89@hotmail.com>
2017-04-27 20:52:33 +0200

    Remove testserver.js

--- testserver.js
+++
@@ -1,9 +0,0 @@
-var io = require('socket.io')();
-
-var socket = io.listen(1000, {});
-
-socket.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    socket.emit('chat message', msg);
-  });
-});
