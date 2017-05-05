de7d00984d3eaaf1c9a3b147b625977fd0ac8f01
Thor <thordoor89@hotmail.com>
2017-05-05 16:07:15 +0200

    Add green square in hrskyen.html on new chat connection

--- index.js
+++ index.js
@@ -8,29 +8,55 @@ var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
-var firstClient = true;
-var firstId = "";
-var roomNo = 1;
+var skyClient = true;
+var skyId = "";
+var skyIds = [];
+var customerIds = [];
 io.on('connection', function (socket) {
-  
-  if (firstClient) {
-    firstId = socket.id;
-    firstClient = false;
+  //push ids from all hrskyen clients to array
+  if (socket.handshake.headers.referer == 'http://localhost:3000/hrskyen.html') {
+    skyId = socket.id;
+    if(!skyIds.includes(skyId)){
+    skyIds.push(skyId);
+    }
+  }
+  else if(!customerIds.includes(socket.id)){
+    console.log("push to customerids " + socket.id);
+    customerIds.push(socket.id);
   }
 });
 
 io.on('connection', function (socket) {
   socket.on('chat message', function (msg) {
-  console.log(socket.id);
-  if(socket.id != firstId){
-    io.to(firstId).emit('chat message', msg);
-  }
-  
-    
-    //socket.broadcast.to(roomName).emit('chat message', msg);
+    //-----------------------------
+    //send from customer to hrskyen
+    //-----------------------------
+    console.log(socket.id);
+    console.log(customerIds);
+    console.log(skyIds);
+    if (!skyIds.includes(socket.id)) {
+      console.log("hej2");
+      for (var i = 0; i < skyIds.length; i++) {
+        skyId = skyIds[i];
+        io.to(skyId).emit('chat message', {msg: msg, id: socket.id});
+      }
+    }
+    /*else if(customerIds.includes(socket.id)){
+      console.log("hej");
+      io.to(socket.id).emit('chat message', msg);
+    }*/
+    //-----------------------------
+    //end
+    //-----------------------------
+    //-----------------------------
+    //send from hrksyen to customer
+    //-----------------------------
+
+    //-----------------------------
+    //end
+    //-----------------------------
   });
 });
-
 http.listen(port, function () {
   console.log('listening on *:' + port);
 });
